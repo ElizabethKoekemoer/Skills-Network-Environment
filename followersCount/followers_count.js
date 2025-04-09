2e6d38f6fc6e4efeef7484f22bc8dadf6794cd49
Elizabeth <emvndm0708@gmail.com>
2025-04-09 17:12:13 +1000

    Practise

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -14,7 +14,13 @@ function checkCountValue() {
     if (count ===10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count ===20) {
-        alert("Your Instagram post gained 20 follers! Keep it up!");
-      
+        alert("Your Instagram post gained 20 follers! Keep it up!");  
+    } else if (count ===0) {
+        alert("Followers count has been reset");
     }
+}
+
+function resetcount() {
+    count=0;
+    displayCount();
 }
