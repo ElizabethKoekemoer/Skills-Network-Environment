9f86bd9bf0c8caf961690a861260840aca4a2d0e
Elizabeth <emvndm0708@gmail.com>
2025-04-14 13:08:27 +1000

    practise

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,31 +1,33 @@
 function submitFeedback() {
-    alert('Thank you for your valuable feedback')
+  alert('Thank you for your valuable feedback')
 
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
-    
-    document.getElementById('userName').innerHTML = username;
-    document.getElementById('userAge').innerHTML = age;
-    document.getElementById('userEmail').innerHTML = email;
-    document.getElementById('userJob').innerHTML = job;
-    document.getElementById('userDesignation').innerHTML = designation;
-    document.getElementById('userProductChoice').innerHTML = productType;
-    document.getElementById('userFeedback').innerHTML = feedback;
-    document.getElementById('userInfo').style.display='block';
-
+  const username = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const email = document.getElementById('email').value;
+  const job = document.getElementById('job').value;
+  const designation = document.getElementById('designation').value;
+  const productType = document.getElementById('productType').value;
+  const feedback = document.getElementById('feedbackText').value;
+  const experince = document.getElementById('Experince').value;  //<!--Practise task--> 
+  
+  document.getElementById('userName').innerHTML = username;
+  document.getElementById('userAge').innerHTML = age;
+  document.getElementById('userEmail').innerHTML = email;
+  document.getElementById('userJob').innerHTML = job;
+  document.getElementById('userDesignation').innerHTML = designation;
+  document.getElementById('userProductChoice').innerHTML = productType;
+  document.getElementById('userFeedback').innerHTML = feedback;
+  document.getElementById('userExperince').innerHTML = experince;  //<!--Practise task-->
+  document.getElementById('userInfo').style.display = 'block';
+  
 }
 
-const submitButton=document.getElementById ('submitBtn');
+const submitButton=document.getElementById('submitButton');
 
-submitButton.onclick =submitFeedback;
+submitButton.onclick = submitFeedback;
 
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      submitFeedback();
-    }
-  });
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    submitFeedback();
+  }
+});
