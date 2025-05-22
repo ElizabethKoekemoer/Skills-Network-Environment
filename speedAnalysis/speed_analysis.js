b97872ef88c9b454df97df9d4fc2b7f8fabcbf60
Elizabeth <emvndm0708@gmail.com>
2025-05-22 13:51:31 +1000

    Practice task

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -25,6 +25,9 @@ function endTest() {
     var timeElapsed = (endTime - startTime) / 1000; //in seconds
     var userTypedText = document.getElementById("userInput").value;
 
+    //Practice task calculate total length 
+    var total = userTypedText.length;
+
     // Split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !=="";
@@ -39,6 +42,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + total + "</p>" +
         "<p>Words Typer: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + 
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
